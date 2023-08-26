abcba4683a9a13d5f4b21f316efb2bb33bc18aa8
OlivTheOlive <111806530+OlivTheOlive@users.noreply.github.com>
2023-08-25 20:56:50 -0400

    learning fundamental concepts: view, texts, images

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("Text clicked")
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Testing React Native fundamentals</Text>
+      <Image 
+      blurRadius={10}
+      source={{
+        width:200,
+        height:300,
+        uri: "https://picsum.photos/200/300",}}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center" 
   },
 });
